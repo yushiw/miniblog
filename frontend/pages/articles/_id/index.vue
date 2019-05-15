98debde5b908ef1310d60407a2518f2ee95c9d16
nek0meshi <nekmeshi@gmail.com>
2019-05-15 22:57:58 +0900

    add mock of articles-detail

---
+++ frontend/pages/articles/_id/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ tags }}</p>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: null,
+      title: '記事1のタイトル',
+      tags: 'ニュース,趣味',
+      created_at: '2019/01/01 01:01:01',
+      updated_at: '2019/01/01 01:01:01',
+      content:
+        '記事の内容記事の内容記事の内容記事の内容記事の内容記事の内容記事の内容'
+    }
+  },
+  computed: {},
+  created() {
+    this.id = this.$route.params.id
+    this.title = '記事' + this.id + 'のタイトル'
+  }
+}
+</script>
