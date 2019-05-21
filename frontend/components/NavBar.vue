b6cc2c479a74c4f447bdc19aac8d6e17c4828eab
y_watanabe <y_watanabe@>
2019-05-21 23:33:42 +0900

    add NavBar

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/articles">MiniBlog</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item v-if="isAdmin" href="#">ユーザー一覧</b-nav-item>
+          <b-nav-item v-if="isLoggedIn" href="#">マイブログ</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-if="isLoggedIn" href="#">ログアウト</b-nav-item>
+          <b-nav-item v-else href="#">ログイン</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isLoggedIn: {
+      type: Boolean,
+      default: false
+    },
+    userType: {
+      // -1: guest, 0: admin, 1: user
+      type: Number,
+      default: -1
+    }
+  },
+  computed: {
+    isAdmin() {
+      return this.userType === 1
+    }
+  }
+}
+</script>
