e7ad09328fb6fb446ac9d93a1a5034312f67143d
nek0meshi <nekmeshi@gmail.com>
2019-05-24 23:42:53 +0900

    update /my/articles/create

--- frontend/pages/my/articles/index.vue
+++ frontend/pages/my/articles/index.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>
+  <div class="container-fluid">
     <NavBar is-logged-in :user-type="1" />
-    <h1>ミニブログ</h1>
-    <b-table hover striped :fields="fields" :items="items">
-      <template slot="title" slot-scope="data">
-        <nuxt-link :to="'/my/articles/' + data.item.id">{{
-          data.value
-        }}</nuxt-link>
-      </template>
-    </b-table>
+    <h1>マイブログ</h1>
+    <div class="row mb-3">
+      <div class="offset-sm-9">
+        <n-link to="/my/articles/create" class="btn btn-primary">
+          ブログを作成する
+        </n-link>
+      </div>
+    </div>
+    <div class="row">
+      <div class="offset-sm-1 col-sm-10">
+        <b-table hover striped :fields="fields" :items="items">
+          <template slot="title" slot-scope="data">
+            <n-link :to="'/my/articles/' + data.item.id">{{
+              data.value
+            }}</n-link>
+          </template>
+        </b-table>
+      </div>
+    </div>
   </div>
 </template>
 
