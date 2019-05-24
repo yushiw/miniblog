41da8533215e54466f5e7bf1b9accda56ee78292
nek0meshi <nekmeshi@gmail.com>
2019-05-24 23:21:24 +0900

    add /my/articles

---
+++ frontend/pages/my/articles/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <NavBar is-logged-in :user-type="1" />
+    <h1>ミニブログ</h1>
+    <b-table hover striped :fields="fields" :items="items">
+      <template slot="title" slot-scope="data">
+        <nuxt-link :to="'/my/articles/' + data.item.id">{{
+          data.value
+        }}</nuxt-link>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar'
+
+export default {
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      fields: [
+        {
+          key: 'title',
+          label: 'タイトル'
+        },
+        {
+          key: 'tags',
+          label: 'タグ'
+        },
+        {
+          key: 'created_at',
+          label: '作成日時'
+        },
+        {
+          key: 'updated_at',
+          label: '更新日時'
+        }
+      ]
+    }
+  },
+  computed: {
+    items() {
+      return [...Array(50).keys()].map(num => ({
+        id: num,
+        title: '記事' + String(num) + 'のタイトル',
+        tags: 'ニュース,趣味',
+        created_at: '2019/01/01 01:01:01',
+        updated_at: '2019/01/01 01:01:01'
+      }))
+    }
+  }
+}
+</script>
