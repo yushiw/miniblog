ff25641416c1c30300ff661a2a9e919a27a4b2f7
y_watanabe <y_watanabe@>
2019-05-26 21:32:08 +0900

    add mocking backend of /articles

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('articles')->group(function () {
+    $controller = 'Web\ArticleController';
+    Route::get('/', $controller . '@index')->name('articles.index');
+    Route::post('/store', $controller . '@store')->name('articles.store');
+    Route::get('/{id}', $controller . '@read')->name('articles.read');
+    Route::post('/{id}/update', $controller . '@update')->name('articles.update');
+    Route::post('/{id}/delete', $controller. '@delete')->name('articles.delete');
+});
