7626716114016392984489be2bb5555b25cfc807
nek0meshi <nekmeshi@gmail.com>
2019-06-03 21:20:00 +0900

    update /articles/_id

--- frontend/pages/articles/_id/index.vue
+++ frontend/pages/articles/_id/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <NavBar is-logged-in :user-type="1" />
-    <h1>{{ title }}</h1>
-    <p>{{ tags }}</p>
-    <p>{{ content }}</p>
+    <div id="content" class="container-fluid">
+      <div class="article">
+        <div class="created_at">
+          {{ created_at }}
+        </div>
+        <h1 class="title">{{ title }}</h1>
+        <div class="tags">
+          <p>{{ tagsText }}</p>
+        </div>
+        <div class="main-text">
+          <p>{{ text }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,18 +28,48 @@ export default {
   data() {
     return {
       id: null,
-      title: '記事1のタイトル',
-      tags: 'ニュース,趣味',
-      created_at: '2019/01/01 01:01:01',
-      updated_at: '2019/01/01 01:01:01',
-      content:
-        '記事の内容記事の内容記事の内容記事の内容記事の内容記事の内容記事の内容'
+      title: '',
+      tags: '',
+      created_at: '',
+      updated_at: '',
+      text: ''
     }
   },
-  computed: {},
-  created() {
-    this.id = this.$route.params.id
-    this.title = '記事' + this.id + 'のタイトル'
+  computed: {
+    tagsText() {
+      return this.tags.map(t => t.name).join(',')
+    }
+  },
+  async asyncData({ $axios, params }) {
+    const res = await $axios.$get(`/backend/articles/${params.id}`)
+    console.log(res)
+    return {
+      title: res.title,
+      tags: res.tags,
+      text: res.text,
+      created_at: res.created_at,
+      updated_at: res.updated_at
+    }
   }
 }
 </script>
+<style lang="scss">
+.created_at {
+  color: $main-color;
+  padding: 0;
+}
+.title {
+  font-size: 2.2rem;
+}
+.article {
+  max-width: 50rem;
+  margin: 1rem auto 0;
+  padding: 1rem 1rem 5rem;
+  border: 2px solid $light-color;
+}
+.tags {
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+</style>
