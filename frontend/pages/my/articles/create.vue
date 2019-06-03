460182d0e3d790652d713f0404672583babb2abc
y_watanabe <y_watanabe@>
2019-06-03 21:19:33 +0900

    fix showing NavBar

--- frontend/pages/my/articles/create.vue
+++ frontend/pages/my/articles/create.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="container-fluid">
+  <div>
     <NavBar is-logged-in :user-type="1" />
-    <h1>新規作成</h1>
-    <div class="row">
-      <div class="offset-sm-1 col-sm-10">
-        <form>
-          <div class="form-group">
-            <label for="titleInput">タイトル</label>
-            <input
-              id="titleInput"
-              v-model="title"
-              type="text"
-              class="form-control"
-              placeholder="タイトル"
-            />
-          </div>
-          <div class="form-group">
-            <label for="textarea">本文</label>
-            <textarea id="textarea" v-model="text" class="form-control" />
-          </div>
-        </form>
+    <div class="container-fluid">
+      <h1>新規作成</h1>
+      <div class="row">
+        <div class="offset-sm-1 col-sm-10">
+          <form>
+            <div class="form-group">
+              <label for="titleInput">タイトル</label>
+              <input
+                id="titleInput"
+                v-model="title"
+                type="text"
+                class="form-control"
+                placeholder="タイトル"
+              />
+            </div>
+            <div class="form-group">
+              <label for="textarea">本文</label>
+              <textarea id="textarea" v-model="text" class="form-control" />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="offset-sm-8">
-        <n-link to="/my/articles" class="btn btn-secondary">
-          キャンセル
-        </n-link>
-        <button type="button" class="btn btn-primary" @click="store">
-          登録
-        </button>
+      <div class="row">
+        <div class="offset-sm-8">
+          <n-link to="/my/articles" class="btn btn-secondary">
+            キャンセル
+          </n-link>
+          <button type="button" class="btn btn-primary" @click="store">
+            登録
+          </button>
+        </div>
       </div>
     </div>
   </div>
